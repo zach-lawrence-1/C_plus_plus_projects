90f1637680c7d2553cd4c2460aa0d83389878dcf
savagecabbage123 <mine268253@gmail.com>
2021-09-14 08:03:05 -0700

    Add files via upload

---
+++ Calc.h
@@ -0,0 +1,9 @@
+#pragma once
+class Calc
+{
+public:
+	double Calculate(double x, char oper, double y);
+	//this is a function which takes in 1 number, 1 operator, and another number
+	/*The double keyword denotes a type of number that supports decimals. 
+	This way, the calculator can handle both decimal math and integer math.*/
+};
