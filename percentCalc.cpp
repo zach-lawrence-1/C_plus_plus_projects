3b87ebd70a7ee44b71c0d0646e5233387f5cbc18
zach_lawrence <mine268253@gmail.com>
2023-12-16 14:47:19 -0600

    Update percentCalc.cpp
    I was looking back at some old projects and noticed I didn't add any user error detection and made a few other modifications.

--- percentCalc.cpp
+++ percentCalc.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<sstream>
 
 struct items
 {
@@ -8,28 +9,53 @@ struct items
 	double amount;
 };
 
-std::vector<items> stuff;
+using std::getline; 
+using std::cin;
 
 int main()
 {
+	std::vector<items> stuff;
 	std::string dataName = "";
 	std::string copy = "";
 	std::string itemName = "";
 	double num = 0;
 	int itemNum = 0;
+
 	std::cout << "please enter what you want to name this collection of percentage data: ";
-	std::cin >> dataName;
-	std::cout << "Please enter the amount of items you want to create a percentage from: ";
+	getline(cin, dataName);
+	std::cout << std::endl << "Please enter the amount of items you want to create a percentage from: ";
 	std::cin >> num;
+
+	while (std::cin.fail())
+	{
+		std::cin.clear();
+		std::cin.ignore();
+		std::cout << std::endl << "Cannot pass non integers";
+		std::cout << std::endl << "Please enter the amount of items you want to create a percentage from: ";
+		std::cin >> num;
+	}
+	std::cin.ignore();
+
 	for (int i = 0; i < num; i++)
 	{
 		std::cout << std::endl << "Please enter the name of this item: ";
-		std::cin >> itemName;
+		getline(cin, itemName);
 		copy = itemName;
 		items itemName;
 		itemName.name = copy;
-		std::cout << std::endl << "Please enter the amount of this item: ";
+		std::cout << "Please enter the amount of this item: ";
 		std::cin >> itemNum;
+
+		while (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << std::endl << "Cannot pass non integers";
+			std::cout << std::endl << "Please enter the amount of this item: ";
+			std::cin >> itemNum;
+		}
+		cin.ignore();
+
 		itemName.amount = itemNum;
 		stuff.push_back(itemName);
 	}
@@ -38,9 +64,9 @@ int main()
 	{
 		num += s.amount;
 	}
-	std::cout << std::endl << std::endl << dataName << std::endl << std::endl << "total amount of items: " << num << std::endl;
+	std::cout << std::endl << std::endl << dataName << std::endl << "total amount of items: " << num << std::endl;
 	for (auto s : stuff)
 	{
-		std::cout << "name: " << s.name << " amount: " << s.amount << " percentage: " << (s.amount/num)*100 << "%" << std::endl;
+		std::cout << "name: " << s.name << "|amount: " << s.amount << " |percentage: " << (s.amount / num) * 100 << "%" << std::endl;
 	}
 }
