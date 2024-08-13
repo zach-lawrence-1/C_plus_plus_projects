02e328270688103598be7171aa754cfe44bd0b4f
zach_lawrence <mine268253@gmail.com>
2024-08-13 11:30:48 -0500

    Add files via upload

---
+++ huffman_coding/huffman.cpp
@@ -0,0 +1,14 @@
+#include "huffman.h"
+
+int main()
+{
+    HuffmanCoding h = HuffmanCoding();
+    string text = "testing of huffman coding 0099100000";
+    cout << "original text: " << endl << text << endl;
+    h.buildTree(text);
+    h.printFrequency();
+    h.printCodes();
+    h.printEncodedString(text);
+    string encodedText = h.getEncodedString(text);
+    h.printDecodedString(encodedText);
+}
