54a7d222f52dfa073a19cc44f9917b51f3b29ddb
zach_lawrence <mine268253@gmail.com>
2024-10-09 16:33:50 -0500

    Update huffman.cpp (#3)
    added some spacing

--- huffman_coding/huffman.cpp
+++ huffman_coding/huffman.cpp
@@ -4,11 +4,15 @@ int main()
 {
     HuffmanCoding h = HuffmanCoding();
     string text = "testing of huffman coding 0099100000";
+    
     cout << "original text: " << endl << text << endl;
+
+    //run required huffman tree functions
     h.buildTree(text);
     h.printFrequency();
     h.printCodes();
     h.printEncodedString(text);
+    
     string encodedText = h.getEncodedString(text);
     h.printDecodedString(encodedText);
 }
